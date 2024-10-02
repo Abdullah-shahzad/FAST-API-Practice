e81c1b778bdb661510c5cfb80f4efe8ab3cec0f4
Abdullah Shahzad <abdullahkpr22@gmail.com>
2024-10-03 01:43:07 +0500

    Develop a user management API

--- models/user.py
+++ models/user.py
@@ -4,11 +4,8 @@ from database.db import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True)
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
-
-
-
